0b8b42ead74f187fc0b9f02bdd6874629b49def3
mridula <mridulaOC@gmail.com>
2025-05-29 21:22:49 +0530

    updated vercel.json

--- client/src/api.ts
+++ client/src/api.ts
@@ -1,12 +1,13 @@
 import axios from "axios";
-
+const API_BASE_URL =
+  import.meta.env.VITE_API_BASE_URL || "https://tasktribe.livewave.ru";
 const adminApi = axios.create({
-  baseURL: `${import.meta.env.VITE_API_BASE_URL}/admin`,
+  baseURL: `${API_BASE_URL}/admin`,
   withCredentials: true,
 });
 
 const userApi = axios.create({
-  baseURL: `${import.meta.env.VITE_API_BASE_URL}`,
+  baseURL: `${API_BASE_URL}`,
   withCredentials: true,
 });
 adminApi.interceptors.request.use((config) => {
